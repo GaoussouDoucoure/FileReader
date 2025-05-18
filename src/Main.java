d4b8efe9e524893f69d302c6574a961cd1fd4caa
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-18 01:13:57 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // How to read a file using Java (3 popular options)
+
+        // BufferedReader + FileReader: Best for reading text files line-by-line
+        // FileInputStream: Best for binary files (e.g., images, audio files)
+        // RandomAccessFile: Best for read/write specific portions of a large file
+
+        String filePath = "Intro.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
         }
     }
 }
